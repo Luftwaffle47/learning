95077664b888f3e6104183ff59b27e3dd4d261dd
Luftwaffle47 <60714356+Luftwaffle47@users.noreply.github.com>
2022-05-04 14:34:35 -0400

    cargame example

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-price = 10
-rating = 4.6
-name = "Daniel"
-is_published = True
-print(price, rating, name, is_published)
+command = ""
+started = False
+while command.lower() !='quit':
+    command = input("> ").lower()
+    if command=='start':
+        if started:
+            print("Your engine begins to skip as you attempt to start while the engine is running")
+        else:
+            started = True
+            print("Car started.....")
+
+    elif command=="stop" :
+        if not started:
+            print("You press on the breaks harder in vain, for the vehicle is already stopped")
+        else:
+            started = False
+            print("Car Stopped")
+
+    elif command == "help":
+        print('''
+        start- to start the car
+        stop- to stop the car
+        quit- to quit the game''')
+    elif command == "quit":
+        break
+    else:
+        print("Sorry I dont quite understand")
+
