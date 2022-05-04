20039d727eed4abc65d7e28c76bab622585f63f7
Luftwaffle47 <60714356+Luftwaffle47@users.noreply.github.com>
2022-05-04 15:05:16 -0400

    cargame example crash possibility

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 command = ""
 started = False
+crash  = False
 while command.lower() !='quit':
     command = input("> ").lower()
     if command=='start':
+        if crash:
+            print("Well, your engine block is busted")
+        started = False
+
         if started:
             print("Your engine begins to skip as you attempt to start while the engine is running")
         else:
@@ -10,17 +15,29 @@ while command.lower() !='quit':
             print("Car started.....")
 
     elif command=="stop" :
+        if crash:
+            started = False
+            print("I don't know what to tell you, but you're stopped already but not by choice")
+
         if not started:
             print("You press on the breaks harder in vain, for the vehicle is already stopped")
         else:
             started = False
             print("Car Stopped")
+    elif command == "crash":
+        if crash == True:
+            print("Wow another vehicle hit you somehow.... ")
+        else:
+            crash = True
+            print("You somehow hit the only tree in the parking lot.....")
+
 
     elif command == "help":
         print('''
         start- to start the car
         stop- to stop the car
-        quit- to quit the game''')
+        quit- to quit the game
+        crash- ded''')
     elif command == "quit":
         break
     else:
